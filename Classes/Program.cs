d4643b63e2b2c63a928021d0c890a9b60353b263
Adedapo Aluko <AlukoA@davidandmargaret.org>
2024-09-03 11:05:49 -0700

    All Done

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Classes
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Classes
 {
     public class Program
     {
+        public class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+            public string Color { get; set; }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Chevrolet";
+            myCar.Model = "Camaro";
+            myCar.Year = 2021;
+            myCar.Color = "Red";
+
+            Console.WriteLine($"My car is a {myCar.Year} {myCar.Make} {myCar.Model} and it is {myCar.Color}.");
+           
         }
     }
 }
